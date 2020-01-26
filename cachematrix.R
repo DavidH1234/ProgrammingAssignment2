41f293ab10a79ab675d89bc48d494d06a4c485a7
DavidH1234 <david.a.heinitz@gmail.com>
2020-01-26 13:19:26 -0500

    Changed cashemaqtrix.R file code and comments to complete the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to calculate the inverse of a 
+## matrix, only if it has not already been done, and then retun
+## the inverted value either from a previously stored state or
+## or after it is calculated and then stored for later retrival.
 
-## Write a short comment describing this function
+## This function creates a list of functions able to be called
+## in the global enviornment by other functions (namely
+## 'casheSolve')
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(matrix) m <<- matrix
+    getmatrix <- function() m
+    list(set = set, get = get, 
+    setmatrix = setmatrix, 
+    getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function takes the list of functions created by 
+## 'makeCacheMatrix' and uses them to first check if the (now
+## global) 'm' variable is emply (NULL).  If it is not, that means
+## the inverse of the desired matrix has already been calculated
+## and does not need to be recalcluated.  Instead it can just be 
+## returned.  However, if 'm' is empty, the matrix inverse has not
+## yet been calculated and needs to be done, using the 'solve()'
+## function and storing it in the matrix object using the 
+## 'setmatrix' function included in the list of functions from 
+## 'makeCacheMatrix'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+        message("getting cashed data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setmatrix(m)
+    m
 }
